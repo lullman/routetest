7f1f98595f45cdcde33f05225ee1d5e32cb27c04
Lindsey Ullman <lindsey.ullman@gmail.com>
2017-05-07 15:23:55 -0700

    Create movie model and migration

--- db/migrate/20170507221145_create_movies.rb
+++ db/migrate/20170507221145_create_movies.rb
@@ -1,6 +1,9 @@
 class CreateMovies < ActiveRecord::Migration
   def change
     create_table :movies do |t|
+      t.string :movie_title
+      t.integer :released
+      t.timestamps
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Movie.create(movie_title: "The Phantom Menace", released: 1999)
+Movie.create(movie_title: "Attack of the Clones", released: 2002)
+Movie.create(movie_title: "Revenge of the Sith", released: 2005)
+Movie.create(movie_title: "A New Hope", released: 1976)
+Movie.create(movie_title: "The Empire Strikes Back", released: 1979)
+Movie.create(movie_title: "Return of the Jedi", released: 1983)
+Movie.create(movie_title: "The Force Awakens", released: 2015)
+Movie.create(movie_title: "The Last Jedi", released: 2017)
+Movie.create(movie_title: "Episode IX", released: 2019)
+Movie.create(movie_title: "Rogue One", released: 2016)
